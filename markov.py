606c2b6f593f65dd3369539d130d06077e647210
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-10-04 15:44:30 -0400

    Improvements according to Matt Harrison's course.

--- markov.py
+++ markov.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 This is a docstring for the markov module. It is a string
 that appears at the top of a Python file.
@@ -7,7 +9,9 @@ that appears at the top of a Python file.
 'b'
 
 """
+import argparse
 import random
+import sys
 import urllib.request as req
 
 
@@ -22,19 +26,47 @@ def write_text(txt, filename, enc='utf8'):
     # done, fout will be closed
 
 
-def get_markov(filename, enc='utf8'):
+def get_markov(filename, enc='utf8', size=1):
     with open(filename, encoding=enc) as fin:
         txt = fin.read()
-    return Markov(txt)
+    return Markov(txt, size)
+
+
+def repl(m):
+    """This gives you a REPL for a Markov chain."""
+    print("Welcome to the REPL. Hit Ctrl-C to exit.")
+    while True:
+        try:
+            txt = input(">")
+        except KeyboardInterrupt:
+            print("Goodbye!")
+            break
+        try:
+            res = m.predict(txt)
+        except KeyError:
+            print("Not found.")
+        except IndexError:
+            print("Whooops. Try again.")
+        else:
+            print(res)
 
 
 class Markov:
-    def __init__(self, data):
-        """This is the constructor docstring"""
-        self.table = get_table(data)
- 
+    def __init__(self, data, size=1):
+        """This is the constructor docstring
+
+        >>> m2 = Markov('abc')
+        >>> m2.predict('b')
+        'c'
+        """
+        self.tables = []
+        for i in range(size):
+            self.tables.append(get_table(data, size=i + 1))
+        # self.table = get_table(data)
+
     def predict(self, data_in):
-        options = self.table.get(data_in, {})
+        table = self.tables[len(data_in) - 1]
+        options = table.get(data_in, {})
         if not options:
             raise KeyError(f"{data_in} not found")
         possibles = []
@@ -45,20 +77,38 @@ class Markov:
         return random.choice(possibles)
 
 
-def get_table(data):
+def get_table(data, size=1):
     """Function docstring
 
     >>> get_table('ab')
     {'a': {'b': 1}}
     """
     results = {}
-    for i, char in enumerate(data):
+    for i, _ in enumerate(data):
+        chars = data[i:i + size]
         try:
-            out = data[i + 1]
+            out = data[i + size]
         except IndexError:
             break
-        char_dict = results.get(char, {})
+        char_dict = results.get(chars, {})
         char_dict.setdefault(out, 0)
         char_dict[out] += 1
-        results[char] = char_dict
+        results[chars] = char_dict
     return results
+
+
+def main(args):
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-f', '--file', help='Input file')
+    ap.add_argument('-s', '--size', help='Markov size',
+                    default=1, type=int)
+    opts = ap.parse_args(args)
+    if opts.file:
+        m = get_markov(opts.file, size=opts.size)
+        repl(m)
+
+
+if __name__ == '__main__':
+    # import doctest
+    # doctest.testmod()
+    main(sys.argv[1:])
--- test_markov.py
+++ test_markov.py
@@ -1,17 +1,27 @@
-#from markov import Markov
-import markov
+from markov import Markov, get_table
+# import markov
 
 import unittest
 
+
 class TestMarkov(unittest.TestCase):
     def test_markov(self):
-        m = markov.Markov("ab")
-        result = m.predict("a")
-        self.assertEqual(result, "b")
+        m = Markov('ab')
+        result = m.predict('a')
+        self.assertEqual(result, 'b')
+
+    def test_markov2(self):
+        m = Markov('abc', size=2)
+        result = m.predict('ab')
+        self.assertEqual(result, 'c')
 
     def test_get_table(self):
-        result = markov.get_table('ab')
+        result = get_table('ab')
         self.assertEqual(result, {'a': {'b': 1}})
+
+    def test_get_table2(self):
+        result = get_table('abc', size=2)
+        self.assertEqual(result, {'ab': {'c': 1}})
 
 
 if __name__ == '__main__':
